83637131531a134e74c234571db001fc94a647f0
Viniciusmq25 <viniciusquintian@gmail.com>
2025-05-20 15:09:18 -0300

    mudei o navbar e as cores

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,21 @@
 	let { children } = $props();
 </script>
 
-<img src="/logo_PET.png" alt="Logo" class="logo" />
 
 <nav class="navbar">
-	<a href="/">Home</a>
-	<a href="/about">Sobre</a>
-	<a href="/members">Membros</a>
-	<a href="/projects">Projetos</a>
-
+  <a href="/">
+    <img src="/logo_PET.png" alt="Logo" class="logo" />
+  </a>
+  <ul class="menu">
+    <a href="/about">Sobre</a>
+    <a href="/about">História</a>
+    <a href="/projects">Projetos</a>
+    <a href="/talks">Talks</a>
+    <a href="/Members">Nossa Equipe</a>
+    <a href="/gallery">Galeria</a>
+    <a href="/contact">Contato</a>
+    <div class="underline"></div>
+  <ul>
 </nav>
 
 {@render children()}
