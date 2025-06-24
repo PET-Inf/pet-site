01c0c2a46eb39635a772b18d0eb8de8a80f1efa0
erick <erickbranquinhomachado@gmail.com>
2025-06-23 23:02:04 -0300

    menu para mobile funcionando

--- README.md
+++ README.md
@@ -1,38 +1,10 @@
-# sv
+# PetInf PUCRS SITE
 
-Everything you need to build a Svelte project, powered by [`sv`](https://github.com/sveltejs/cli).
+## Instruções 
+1 - Instalar o nodejs e npm
 
-## Creating a project
+2 - Clonar repositório
 
-If you're seeing this, you've probably already done this step. Congrats!
+3 - Executar o comando `npm install` para instalar dependências do projeto
 
-```bash
-# create a new project in the current directory
-npx sv create
-
-# create a new project in my-app
-npx sv create my-app
-```
-
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
-
-```bash
-npm run dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
-```
-
-## Building
-
-To create a production version of your app:
-
-```bash
-npm run build
-```
-
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
+4 - Executar o comando `npm run dev` para executar o código.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,22 +8,47 @@
 	import Equipe from '$lib/sections/Equipe.svelte';
 	import Galeria from '$lib/sections/Galeria.svelte';
 	import Contato from '$lib/sections/Contato.svelte';
+
+  let menuOpen = false
+
+  const toggleMenu = () => menuOpen = !menuOpen;
+  const closeMenu = () => menuOpen = false;
 </script>
 
+
 <nav class="navbar">
-	<a href="#inicio">
-		<img src="/logo_PET.png" alt="Logo" class="logo" />
-	</a>
-	<ul class="menu">
-		<a href="#sobre">Sobre</a>
-		<a href="#projetos">Projetos</a>
-		<a href="#historia">História</a>
-		<a href="#equipe">Nossa Equipe</a>
-		<a href="#galeria">Galeria</a>
+  <!-- Menu dropdown para mobile -->
+  <div class="menu-toggle-wrapper">
+    <button class="menu-toggle" on:click={toggleMenu}>☰</button>
+    {#if menuOpen}
+      <div class="dropdown">
+        <a href="#sobre" on:click={closeMenu}>Sobre</a>
+        <a href="#projetos" on:click={closeMenu}>Projetos</a>
+        <a href="#historia" on:click={closeMenu}>História</a>
+        <a href="#equipe" on:click={closeMenu}>Nossa Equipe</a>
+        <a href="#galeria" on:click={closeMenu}>Galeria</a>
+        <a href="#talks" on:click={closeMenu}>Talks</a>
+        <a href="#contato" on:click={closeMenu}>Contato</a>
+      </div>
+    {/if}
+  </div>
+
+  <a href="#inicio">
+    <img src="/logo_PET.png" alt="Logo" class="logo" />
+  </a>
+
+  <!-- Menu horizontal desktop -->
+  <ul class="menu">
+    <a href="#sobre">Sobre</a>
+    <a href="#projetos">Projetos</a>
+    <a href="#historia">História</a>
+    <a href="#equipe">Nossa Equipe</a>
+    <a href="#galeria">Galeria</a>
     <a href="#talks">Talks</a>
-		<a href="#contato">Contato</a>
-	</ul>
+    <a href="#contato">Contato</a>
+  </ul>
 </nav>
+
 
 <main>
   <section id = "inicio">
@@ -51,5 +76,6 @@
     <Contato />
   </section>
 </main>
+
 
 
