725dcd5589846cf1dd04c56378aafa0d8b17eab9
Vinícius Ross <108466053+viniross@users.noreply.github.com>
2025-08-21 15:35:24 -0300

    navbar in a unique archive to import for other screens

--- src/routes/selecao/+page.svelte
+++ src/routes/selecao/+page.svelte
@@ -1,1 +1,7 @@
-<h1> Detalhes da inscricao no processo seletivo do PET </h1>
+<script> import Navbar from '$lib/components/Navbar.svelte';</script>
+
+<Navbar />
+
+<main>
+  <h1> Detalhes da inscricao no processo seletivo do PET </h1>
+</main>
