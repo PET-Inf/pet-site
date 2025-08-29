d3da0299c5e83c3ee26da035a8f6ea621f87d417
Vinícius Ross <108466053+viniross@users.noreply.github.com>
2025-08-29 17:48:45 -0300

    html for "historia" structured

--- src/lib/sections/Historia.svelte
+++ src/lib/sections/Historia.svelte
@@ -1,1 +1,44 @@
-<h1> Informacoes sobre a historia do PET </h1>
+<style>
+    .container {
+        background-color: #1A447C;
+        color: #FFFFFF;
+    }
+</style>
+
+<div class="container">
+    <h2 class="text-3xl md:text-4xl font-bold text-center mb-8 md:mb-12">História</h2>
+
+    <div class="timeline">
+        <div class="timeline-item start left">
+            <div class="timeline-content">
+                <h3>Novembro de 1991</h3>
+                <h4>Criação do PET-Informática</h4>
+                <p>O PET-Inf foi criado na PUCRS ao final de 1991, tendo como primeiro tutor o professor Álvaro Guarda.</p>
+            </div>
+            <div class="timeline-circle image - 1"></div>
+        </div>
+
+        <div class="timeline-item right">
+            <div class="timeline-circle image-2"></div>
+                <div class="timeline-content">
+                    <h3>1991 - Hoje</h3>
+                    <h4>Tutores do PET-Info</h4>
+                    <ul>
+                        <li>Prof. Dr. Álvaro Guarda: 1991-1993</li>
+                        <li>Prof. Dr. Afonso Orth: 1993-1996</li>
+                        <li>Prof. Dr. Celso Maciel: 1996-2000 | 2010-2011</li>
+                        <li>Prof. Dr. Fabiano Hessel: 2001-2002</li>
+                        <li>Prof. Dr. Luís Lamb: 2002</li>
+                        <li>Profa. Dra. Lúcia Giraffa: 2002-2005</li>
+                        <li>Prof. Dr. Alfio Martini: 2005-2010 | 2018</li>
+                        <li>Prof. Dr. Rafael Garibotti: 2019</li>
+                        <li>Prof. Dr. Tiago Ferreto: 2011-2017 | 2020-2023</li>
+                        <li>Profa. Dra. Milene Silveira: 2023-Atual</li>
+                    </ul>
+                </div>
+        </div>
+        <div class="timeline-bottom-circle">
+            <p>Faça parte da nossa história!</p>
+        </div>
+    </div>
+</div>
